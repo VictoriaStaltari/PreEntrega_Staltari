f9e24dd1a45185d6b64ebfba08540e70089a79f3
Victoria Staltari <victoriastaltari19@gmail.com>
2025-06-04 19:26:51 -0300

    correcion del .env para variables de entorno y agregar routes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import mocksRouter from './routes/mocks.router.js';
-
+import usersRouter from './routes/users.router.js';
+import petsRouter from './routes/pets.router.js';
 
 dotenv.config();
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
+app.use('api/users',usersRouter)
+app.use('api/pets',petsRouter)
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('MongoDB Conectado'))
-.catch(err => console.error('Error de conexión:', err));
+.catch(err => console.error('Error de conexión:', err))
 
-app.use('/api/mocks', mocksRouter);
+app.use('/api/mocks', mocksRouter)
 
-app.listen(3000, () => console.log('Server listening in: 3000'));
+app.listen(3000, () => console.log('Server listening in: 3000'))
